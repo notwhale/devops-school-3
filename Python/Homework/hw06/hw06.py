b5a80b2801ab9516c94b0a5482ea0badab20cd09
Nikita Kapishev <notwhale@gmail.com>
2021-08-02 17:01:19 +0500

    add pyhton homework 01-06

---
+++ Python/Homework/hw06/hw06.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+https://projecteuler.net/problem=36
+
+The decimal number, 585 = 1001001001 in binary, is palindromic in both bases.
+Find the sum of all numbers, less than one million, which are palindromic in
+base 10 and base 2. (Please note that the palindromic number,
+in either base, may not include leading zeros.)
+
+Напишите программу, которая решает описанную выше задачу и печатает ответ.
+"""
+
+summ = 0
+for decimal in range(1, 1000001):
+    if str(decimal) == str(decimal)[::-1]:
+        binary = format(decimal, '020b')
+        if binary == binary[::-1]:
+            summ += decimal
+print(summ)
