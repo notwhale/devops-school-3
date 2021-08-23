c58b55c6fb792ae54810b897a5e02a09e66eb021
Nikita Kapishev <notwhale@gmail.com>
2021-08-23 17:08:40 +0500

    Modified hw11

--- Python/Homework/hw11/hw11.py
+++ Python/Homework/hw11/hw11.py
@@ -33,8 +33,9 @@ def letters_range(start, stop, step=1):
     """
     return [chr(_) for _ in range(ord(start), ord(stop), step)]
 
-print(letters_range('b', 'w', 2))
-print(letters_range('a', 'g'))
-print(letters_range('g', 'p'))
-print(letters_range('p', 'g', -2))
-print(letters_range('a', 'a'))
+if __name__ == "__main__":
+    print(letters_range('b', 'w', 2))
+    print(letters_range('a', 'g'))
+    print(letters_range('g', 'p'))
+    print(letters_range('p', 'g', -2))
+    print(letters_range('a', 'a'))
